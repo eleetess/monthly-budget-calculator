2573b46d6b23e403abdae5bcab22f2583b8fa80e
eleetess <ericaleetesseo@gmail.com>
2025-05-06 19:23:31 -0500

    adding links to websites, adding row and col, more images, setting up js variables

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 //get values from the input fields
 let bun="hello world"
 console.log(bun)
+let myForm=document.getElementById
+let incomeAmount=document.getElementById
+let rent=document.getElementById
+let electric=document.getElementById
+let water=document.getElementById
+let gas=document.getElementById
+let internet=document.getElementById
+let insurance=document.getElementById
+let groceries=document.getElementById
+let childcare=document.getElementById
+
+//stuck: i have the values, how do i start the process of getting utilities subtracted from the income amount
+//to give me the ending total i need do i need to create a class for the utilities or do i need to put ids again in 
+//
+
+
 //check if the inputs are valid numbers
 //calculate the remaining balance
 //display the result
